4630bc80f1922406eedaa54bfcd360745aea39aa
LeAvNa <130793158+LeAvNa@users.noreply.github.com>
2024-09-14 12:20:36 -0700

    Modificar estructura sidebar

--- proyecto_topicos/src/constants/EstructuraMenu.js
+++ proyecto_topicos/src/constants/EstructuraMenu.js
@@ -5,7 +5,7 @@ export const EstructuraMenu = {
     "items": [
       {
         "etiqueta": "Inicio",
-        "Path": "/", 
+        "Path": "/",
         "icon": "mdi-home",
         "Hijos": []
       }
@@ -13,56 +13,64 @@ export const EstructuraMenu = {
   },
 
   "Sucursales": {
-    "etiqueta": "Sucursal #1",
+    "etiqueta": "Sucursales",
     "items": [
       {
-        "etiqueta": "Sucursal",
-        "Path": "/sucursal",  
+        "etiqueta": "Sucursal 1",
+        "Path": "",
         "icon": "mdi mdi-storefront",
-        "Hijos": []
+        "Hijos": [
+          {
+            "etiqueta": "Sucursal",
+            "Path": "/sucursal",
+            "icon": "mdi mdi-storefront",
+            "Hijos": []
+          },
+          {
+            "etiqueta": "Clientes",
+            "Path": "/clientes",
+            "icon": "mdi-account",
+            "Hijos": []
+          },
+          {
+            "etiqueta": "Empleados",
+            "Path": "/empleados",
+            "icon": "mdi-account-group",
+            "Hijos": []
+          },
+          {
+            "etiqueta": "Ventas",
+            "Path": "/ventas",
+            "icon": "mdi-cart",
+            "Hijos": []
+          },
+          {
+            "etiqueta": "Cocteleria",
+            "Path": "/cocteleria",
+            "icon": "mdi-glass-cocktail",
+            "Hijos": []
+          },
+          {
+            "etiqueta": "Producto",
+            "Path": "/producto",
+            "icon": "mdi-package-variant",
+            "Hijos": []
+          },
+          {
+            "etiqueta": "Proveedor",
+            "Path": "/proveedor",
+            "icon": "mdi-truck",
+            "Hijos": []
+          },
+          {
+            "etiqueta": "Inventario",
+            "Path": "/inventario",
+            "icon": "mdi-clipboard-list",
+            "Hijos": []
+          }
+        ]
       },
-      {
-        "etiqueta": "Clientes",
-        "Path": "/clientes",
-        "icon": "mdi-account",
-        "Hijos": []
-      },
-      {
-        "etiqueta": "Empleados",
-        "Path": "/empleados",
-        "icon": "mdi-account-group",
-        "Hijos": []
-      },
-      {
-        "etiqueta": "Ventas",
-        "Path": "/ventas",
-        "icon": "mdi-cart",
-        "Hijos": []
-      },
-      {
-        "etiqueta": "Cocteleria",
-        "Path": "/cocteleria",
-        "icon": "mdi-glass-cocktail",
-        "Hijos": []
-      },
-      {
-        "etiqueta": "Producto",
-        "Path": "/producto",
-        "icon": "mdi-package-variant",
-        "Hijos": []
-      },
-      {
-        "etiqueta": "Proveedor",
-        "Path": "/proveedor",
-        "icon": "mdi-truck",
-        "Hijos": []
-      },
-      {
-        "etiqueta": "Inventario",
-        "Path": "/inventario",
-        "icon": "mdi-clipboard-list",
-        "Hijos": []
-      }
+
     ]
   },
   // Añade otras categorías y elementos adicionales según sea necesario
